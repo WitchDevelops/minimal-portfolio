86b0f2ca962e2b19f9ec7dae6de7f4e3a5e2d3f1
Dominika Wojewska <112077394+WitchDevelops@users.noreply.github.com>
2023-09-22 18:31:07 +0200

    #12 added src to images in the project slider

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,3 +1,4 @@
+// for the overview
 import desktopManage2x from "../assets/images/portfolio/desktop/image-portfolio-manage@2x.jpg";
 import tabletManage from "../assets/images/portfolio/tablet/image-portfolio-manage.jpg";
 import mobileManage from "../assets/images/portfolio/mobile/image-portfolio-manage.jpg";
@@ -14,6 +15,47 @@ import desktopFylo2x from "../assets/images/portfolio/desktop/image-portfolio-fy
 import tabletFylo from "../assets/images/portfolio/tablet/image-portfolio-fylo.jpg";
 import mobileFylo from "../assets/images/portfolio/mobile/image-portfolio-fylo.jpg";
 
+//for the detailed view
+  //Manage
+import heroDesktopManage from "../assets/images/detail/desktop/image-manage-hero@2x.jpg";
+import heroTabletManage from "../assets/images/detail/tablet/image-manage-hero@2x.jpg";
+import heroMobileManage from "../assets/images/detail/mobile/image-manage-hero@2x.jpg";
+import prev1DesktopManage from "../assets/images/detail/desktop/image-manage-preview-1@2x.jpg";
+import prev2DesktopManage from "../assets/images/detail/desktop/image-manage-preview-2@2x.jpg";
+import prev1TabletManage from "../assets/images/detail/tablet/image-manage-preview-1@2x.jpg";
+import prev2TabletManage from "../assets/images/detail/tablet/image-manage-preview-2@2x.jpg";
+import prev1MobileManage from "../assets/images/detail/mobile/image-manage-preview-1@2x.jpg";
+import prev2MobileManage from "../assets/images/detail/mobile/image-manage-preview-2@2x.jpg";
+  //Bookmark
+  import heroDesktopBookmark from "../assets/images/detail/desktop/image-bookmark-hero@2x.jpg";
+  import heroTabletBookmark from "../assets/images/detail/tablet/image-bookmark-hero@2x.jpg";
+  import heroMobileBookmark from "../assets/images/detail/mobile/image-bookmark-hero@2x.jpg";
+  import prev1DesktopBookmark from "../assets/images/detail/desktop/image-bookmark-preview-1@2x.jpg";
+  import prev2DesktopBookmark from "../assets/images/detail/desktop/image-bookmark-preview-2@2x.jpg";
+  import prev1TabletBookmark from "../assets/images/detail/tablet/image-bookmark-preview-1@2x.jpg";
+  import prev2TabletBookmark from "../assets/images/detail/tablet/image-bookmark-preview-2@2x.jpg";
+  import prev1MobileBookmark from "../assets/images/detail/mobile/image-bookmark-preview-1@2x.jpg";
+  import prev2MobileBookmark from "../assets/images/detail/mobile/image-bookmark-preview-2@2x.jpg";
+    //Insure
+import heroDesktopInsure from "../assets/images/detail/desktop/image-insure-hero@2x.jpg";
+import heroTabletInsure from "../assets/images/detail/tablet/image-insure-hero@2x.jpg";
+import heroMobileInsure from "../assets/images/detail/mobile/image-insure-hero@2x.jpg";
+import prev1DesktopInsure from "../assets/images/detail/desktop/image-insure-preview-1@2x.jpg";
+import prev2DesktopInsure from "../assets/images/detail/desktop/image-insure-preview-2@2x.jpg";
+import prev1TabletInsure from "../assets/images/detail/tablet/image-insure-preview-1@2x.jpg";
+import prev2TabletInsure from "../assets/images/detail/tablet/image-insure-preview-2@2x.jpg";
+import prev1MobileInsure from "../assets/images/detail/mobile/image-insure-preview-1@2x.jpg";
+import prev2MobileInsure from "../assets/images/detail/mobile/image-insure-preview-2@2x.jpg";
+  //Fylo
+  import heroDesktopFylo from "../assets/images/detail/desktop/image-fylo-hero@2x.jpg";
+  import heroTabletFylo from "../assets/images/detail/tablet/image-fylo-hero@2x.jpg";
+  import heroMobileFylo from "../assets/images/detail/mobile/image-fylo-hero@2x.jpg";
+  import prev1DesktopFylo from "../assets/images/detail/desktop/image-fylo-preview-1@2x.jpg";
+  import prev2DesktopFylo from "../assets/images/detail/desktop/image-fylo-preview-2@2x.jpg";
+  import prev1TabletFylo from "../assets/images/detail/tablet/image-fylo-preview-1@2x.jpg";
+  import prev2TabletFylo from "../assets/images/detail/tablet/image-fylo-preview-2@2x.jpg";
+  import prev1MobileFylo from "../assets/images/detail/mobile/image-fylo-preview-1@2x.jpg";
+  import prev2MobileFylo from "../assets/images/detail/mobile/image-fylo-preview-2@2x.jpg";
 
 const projects = [
     {
@@ -30,7 +72,15 @@ const projects = [
       tag: "Interaction Design / Front End Development",
       tech: "HTML / CSS / JS",
       background: "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
-      
+      heroDesktop: `${heroDesktopManage}`,
+      heroTablet: `${heroTabletManage}`,
+      heroMobile: `${heroMobileManage}`,
+      preview1Desktop: `${prev1DesktopManage}`,
+      preview2Desktop: `${prev2DesktopManage}`,
+      preview1Tablet: `${prev1TabletManage}`,
+      preview2Tablet: `${prev2TabletManage}`,
+      preview1Mobile: `${prev1MobileManage}`,
+      preview2Mobile: `${prev2MobileManage}`,
     },
     {
       id: "bookmark",
@@ -46,6 +96,15 @@ const projects = [
       tag: "Interaction Design / Front End Development",
       tech: "HTML / CSS / JS",
       background: "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+      heroDesktop: `${heroDesktopBookmark}`,
+      heroTablet: `${heroTabletBookmark}`,
+      heroMobile: `${heroMobileBookmark}`,
+      preview1Desktop: `${prev1DesktopBookmark}`,
+      preview2Desktop: `${prev2DesktopBookmark}`,
+      preview1Tablet: `${prev1TabletBookmark}`,
+      preview2Tablet: `${prev2TabletBookmark}`,
+      preview1Mobile: `${prev1MobileBookmark}`,
+      preview2Mobile: `${prev2MobileBookmark}`,
     },
     {
       id: "insure",
@@ -61,6 +120,15 @@ const projects = [
       tag: "Interaction Design / Front End Development",
       tech: "HTML / CSS / JS",
       background: "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+      heroDesktop: `${heroDesktopInsure}`,
+      heroTablet: `${heroTabletInsure}`,
+      heroMobile: `${heroMobileInsure}`,
+      preview1Desktop: `${prev1DesktopInsure}`,
+      preview2Desktop: `${prev2DesktopInsure}`,
+      preview1Tablet: `${prev1TabletInsure}`,
+      preview2Tablet: `${prev2TabletInsure}`,
+      preview1Mobile: `${prev1MobileInsure}`,
+      preview2Mobile: `${prev2MobileInsure}`,
     },
     {
       id: "fylo",
@@ -76,6 +144,15 @@ const projects = [
       tag: "Interaction Design / Front End Development",
       tech: "HTML / CSS ",
       background: "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+      heroDesktop: `${heroDesktopFylo}`,
+      heroTablet: `${heroTabletFylo}`,
+      heroMobile: `${heroMobileFylo}`,
+      preview1Desktop: `${prev1DesktopFylo}`,
+      preview2Desktop: `${prev2DesktopFylo}`,
+      preview1Tablet: `${prev1TabletFylo}`,
+      preview2Tablet: `${prev2TabletFylo}`,
+      preview1Mobile: `${prev1MobileFylo}`,
+      preview2Mobile: `${prev2MobileFylo}`,
     },
     
   ];
