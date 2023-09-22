534ee276787e6bf4ffecdd7d220e49ee51d06a89
Dominika Wojewska <112077394+WitchDevelops@users.noreply.github.com>
2023-09-22 16:31:56 +0200

    #12 add slider effect and buttons

--- src/pages/ProjectSlider.js
+++ src/pages/ProjectSlider.js
@@ -1,20 +1,54 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import projects from "../data/projectsData.js";
 
 function ProjectSlider() {
     const { projectId } = useParams();
-    const project = projects.find((p) => p.id === projectId);
+    const navigate = useNavigate();
   
-    if (!project) {
+    const initialProjectIndex = projects.findIndex((project) => project.id === projectId);
+    const [currentProjectIndex, setCurrentProjectIndex] = useState(initialProjectIndex);
+  
+    const goToProject = (index) => {
+      const project = projects[index];
+      setCurrentProjectIndex(index);
+      navigate(`/portfolio/${project.id}`);
+    };
+  
+    useEffect(() => {
+      setCurrentProjectIndex(initialProjectIndex);
+      window.scrollTo(0, 0);
+    }, [projectId, initialProjectIndex]);
+  
+    const currentProject = projects[currentProjectIndex];
+    const previousProjectIndex = (currentProjectIndex - 1 + projects.length) % projects.length;
+    const nextProjectIndex = (currentProjectIndex + 1) % projects.length;
+    const previousProject = projects[previousProjectIndex];
+    const nextProject = projects[nextProjectIndex];
+  
+    if (!currentProject) {
       return <div>Project not found</div>;
     }
   
     return (
       <div>
-        {/* Render your project slider here using project data */}
-        <h1>{project.title} Slider</h1>
-        {/* Add slider and buttons here */}
+        <h1>{currentProject.title}</h1>
+        
+        {/* Add content specific to the current project */}
+        <p>{currentProject.description}</p>
+        <img src={currentProject.imageUrl} alt={currentProject.alt} />
+  
+        {/* Buttons for Previous and Next Project */}
+        <div>
+          <button onClick={() => goToProject(previousProjectIndex)}>
+            &larr; {previousProject.title}
+            <span>Previous Project</span>
+          </button>
+          <button onClick={() => goToProject(nextProjectIndex)}>
+            {nextProject.title} &rarr;
+            <span>Next Project</span>
+          </button>
+        </div>
       </div>
     );
   }
