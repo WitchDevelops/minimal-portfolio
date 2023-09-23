0bb44d16b06981f049052028bd3027bc1547d45d
Dominika Wojewska <112077394+WitchDevelops@users.noreply.github.com>
2023-09-23 09:46:57 +0200

    #12 desktop layout project slider

--- src/pages/ProjectSlider.js
+++ src/pages/ProjectSlider.js
@@ -50,77 +50,83 @@ function ProjectSlider() {
           alt=""
         />
       </picture>
+      <div className={styles.desktop_wrapper}>
+        <div className={styles.header_wrapper}>
+          <div className={styles.project_details}>
+            <h2 className={styles.section__header}>{currentProject.title}</h2>
 
-      <div className={styles.header_wrapper}>
-        <div className={styles.project_details}>
-          <h2 className={styles.section__header}>{currentProject.title}</h2>
-
-          <div className={styles.tag_btn_wrapper}>
-            <div className={styles.tags}>
-              <p className={styles.accent}>{currentProject.tag}</p>
-              <p className={styles.accent}>{currentProject.tech}</p>
+            <div className={styles.tag_btn_wrapper}>
+              <div className={styles.tags}>
+                <p className={styles.accent}>{currentProject.tag}</p>
+                <p className={styles.accent}>{currentProject.tech}</p>
+              </div>
+              <a
+                href="https://github.com/"
+                target="_blank"
+                rel="noreferrer"
+                className={`${styles.button} ${styles.button_secondary}`}
+              >
+                visit website
+              </a>
             </div>
-            <a
-              href="https://github.com/"
-              target="_blank"
-              rel="noreferrer"
-              className={`${styles.button} ${styles.button_secondary}`}
-            >
-              visit website
-            </a>
+
+            <p className={styles.description_txt}>
+              {currentProject.description}
+            </p>
           </div>
-
-          <p className={styles.description_txt}>{currentProject.description}</p>
+        </div>
+        <div>
+          <div className={styles.detail_wrapper}>
+            <h3 className={styles.section__subheader}>Project Background</h3>
+            <p className={styles.description_txt}>
+              {currentProject.background}
+            </p>
+          </div>
+          <div className={styles.preview_wrapper}>
+            <h3 className={styles.section__subheader}>Static Previews</h3>
+            <div classname={styles.preview_images}>
+              <picture className={`${styles.img_fluid} ${styles.preview_img}`}>
+                <source
+                  srcset={currentProject.preview1Desktop}
+                  media="(min-width: 768px)"
+                />
+                <source
+                  srcset={currentProject.preview1Tablet}
+                  media="(min-width: 576px)"
+                />
+                <source
+                  srcset={currentProject.preview1Mobile}
+                  media="(max-width: 575px)"
+                />
+                <img
+                  className={`${styles.img_fluid} ${styles.preview_img}`}
+                  src={currentProject.preview1Mobile}
+                  alt=""
+                />
+              </picture>
+              <picture className={styles.img_fluid}>
+                <source
+                  srcset={currentProject.preview2Desktop}
+                  media="(min-width: 768px)"
+                />
+                <source
+                  srcset={currentProject.preview2Tablet}
+                  media="(min-width: 576px)"
+                />
+                <source
+                  srcset={currentProject.preview2Mobile}
+                  media="(max-width: 575px)"
+                />
+                <img
+                  className={styles.img_fluid}
+                  src={currentProject.preview2Mobile}
+                  alt=""
+                />
+              </picture>
+            </div>
+          </div>
         </div>
       </div>
-      <div className={styles.detail_wrapper}>
-        <h3 className={styles.section__subheader}>Project Background</h3>
-        <p className={styles.description_txt}>{currentProject.background}</p>
-      </div>
-      <div className={styles.preview_wrapper}>
-        <h3 className={styles.section__subheader}>Static Previews</h3>
-        <div classname={styles.preview_images}>
-          <picture className={`${styles.img_fluid} ${styles.preview_img}`}>
-            <source
-              srcset={currentProject.preview1Desktop}
-              media="(min-width: 768px)"
-            />
-            <source
-              srcset={currentProject.preview1Tablet}
-              media="(min-width: 576px)"
-            />
-            <source
-              srcset={currentProject.preview1Mobile}
-              media="(max-width: 575px)"
-            />
-            <img
-              className={`${styles.img_fluid} ${styles.preview_img}`}
-              src={currentProject.preview1Mobile}
-              alt=""
-            />
-          </picture>
-          <picture className={styles.img_fluid}>
-            <source
-              srcset={currentProject.preview2Desktop}
-              media="(min-width: 768px)"
-            />
-            <source
-              srcset={currentProject.preview2Tablet}
-              media="(min-width: 576px)"
-            />
-            <source
-              srcset={currentProject.preview2Mobile}
-              media="(max-width: 575px)"
-            />
-            <img
-              className={styles.img_fluid}
-              src={currentProject.preview2Mobile}
-              alt=""
-            />
-          </picture>
-        </div>
-      </div>
-
       {/* Buttons for Previous and Next Project */}
       <div className={styles.nav_btns_wrapper}>
         <button
@@ -141,7 +147,6 @@ function ProjectSlider() {
             <p className={styles.btn_title}>{previousProject.title}</p>
             <p className={styles.btn_nav_txt}>Previous Project</p>
           </div>
-          
         </button>
         <button
           className={styles.nav_btn}
@@ -158,8 +163,9 @@ function ProjectSlider() {
             <path d="M1 1L9 9L1 17" stroke="#33323D" />
           </svg>
           <div className={styles.btn_txt_wrapper}>
-          <p className={styles.btn_title}>{nextProject.title}</p>
-          <p className={styles.btn_nav_txt}>Next Project</p></div>
+            <p className={styles.btn_title}>{nextProject.title}</p>
+            <p className={styles.btn_nav_txt}>Next Project</p>
+          </div>
         </button>
       </div>
     </section>
