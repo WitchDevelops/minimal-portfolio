4eb9d18bf731a254578ad44aec3546452796d6bf
Dominika Wojewska <112077394+WitchDevelops@users.noreply.github.com>
2023-09-23 20:46:54 +0200

    #4 add email validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,73 +1,96 @@
-import React, { useState } from 'react';
-import styles from './Form.module.css';
+import React, { useState } from "react";
+import styles from "./Form.module.css";
 
 function Form() {
-    //define variables for the input fields
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
+  //define variables for the input fields
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [emailError, setEmailError] = useState('');
+  const validateEmail = (email) => {
+    const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return emailPattern.test(email);
+  };
 
-    //handle form submission
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleEmailChange = (e) => {
+    const newEmail = e.target.value;
+    setEmail(newEmail);
 
-        
-        
+    // Check if the new email is valid and clear the error message if it is
+    if (validateEmail(newEmail)) {
+      setEmailError("");
+    }
+  };
+  //handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+        setEmailError('Please enter a valid email address.');
+        return;
+      }
+    //TODO form submisstion logic
 
-        //clear form fields after submission
-        setName('');
-        setEmail('');
-        setMessage('');
-    };
+    //clear form fields after submission
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
 
-    return (
-        <form className={styles.form_wrapper} onSubmit={handleSubmit}>
-            <div className={styles.form_group}>
-               <label htmlFor="name" className={styles.form_label}>Name</label>
-            <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Jane Appleseed"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className={styles.form_input}
-                required
-                >
-            </input> 
-            </div>
-            <div className={styles.form_group}>
-               <label htmlFor="email" className={styles.form_label}>Email Address</label>
-            <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="email@example.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className={styles.form_input}
-                required
-                >
-            </input> 
-            </div>
-            <div className={styles.form_group}>
-               <label htmlFor="message" className={styles.form_label}>Message</label>
-            <textarea 
-                id="message"
-                name="message"
-                placeholder="How can I help?"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                className={styles.form_input}
-                required
-                >
+  return (
+    <form className={styles.form_wrapper} onSubmit={handleSubmit}>
+      <div className={styles.form_group}>
+        <label htmlFor="name" className={styles.form_label}>
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Jane Appleseed"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className={styles.form_input}
+          required
+        ></input>
+      </div>
+      <div className={styles.form_group}>
+        <label htmlFor="email" className={styles.form_label}>
+          Email Address
+        </label>
+        <input
+          id="email"
+          name="email"
+          placeholder="email@example.com"
+          value={email}
+          onChange={handleEmailChange}
+          className={`${styles.form_input} ${emailError ? styles.error_input : ''}`}
+          required
+        ></input>
+        {emailError && <p className={styles.error}>{emailError}</p>}
+      </div>
+      <div className={styles.form_group}>
+        <label htmlFor="message" className={styles.form_label}>
+          Message
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          placeholder="How can I help?"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className={styles.form_input}
+          required
+        ></textarea>
+      </div>
 
-            </textarea> 
-            </div>
-            
-            <button type="submit" className={`${styles.button} ${styles.button__text} ${styles.form_button}`}>send message</button>
-        </form>
-    )
+      <button
+        type="submit"
+        className={`${styles.button} ${styles.button__text} ${styles.form_button}`}
+      >
+        send message
+      </button>
+    </form>
+  );
 }
 
 export default Form;
