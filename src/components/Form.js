cff89a9f97f552cf3d0298d30962a16dcb1a42f6
Dominika Wojewska <112077394+WitchDevelops@users.noreply.github.com>
2023-09-23 21:01:07 +0200

    #4 add form success and resubmission

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,11 +7,14 @@ function Form() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [emailError, setEmailError] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  //email validation
   const validateEmail = (email) => {
     const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     return emailPattern.test(email);
   };
-
+  //to update the erorr state when user types into the input field
   const handleEmailChange = (e) => {
     const newEmail = e.target.value;
     setEmail(newEmail);
@@ -30,14 +33,32 @@ function Form() {
       }
     //TODO form submisstion logic
 
-    //clear form fields after submission
+    //clear form fields after submission ans set the submit state to true
+    setIsSubmitted(true);
     setName("");
     setEmail("");
     setMessage("");
   };
 
+  //allow to show the contact form again after submission
+  const handleResetForm = () => {
+    setIsSubmitted(false);
+  };
+
   return (
-    <form className={styles.form_wrapper} onSubmit={handleSubmit}>
+    <div>
+        {isSubmitted ? (
+            <div>
+                <p className={styles.success}>Your message has been successfully submitted!</p>
+                <button
+              onClick={handleResetForm}
+              className={`${styles.button} ${styles.button__text} ${styles.form_button_again}`}
+            >
+              Submit Another Message
+            </button>
+            </div>
+        ) : (
+            <form className={styles.form_wrapper} onSubmit={handleSubmit}>
       <div className={styles.form_group}>
         <label htmlFor="name" className={styles.form_label}>
           Name
@@ -90,6 +111,9 @@ function Form() {
         send message
       </button>
     </form>
+        )}
+    </div>
+    
   );
 }
 
