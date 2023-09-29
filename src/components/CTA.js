a171aa9971b1a5e56875ff63a92529868b7333bf
WitchDevelops <112077394+WitchDevelops@users.noreply.github.com>
2023-09-29 19:48:55 +0200

    add scroll to top to btn

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./CTA.module.css";
 
 function CTA() {
+  const navigate = useNavigate();
+  const handleContactLinkClick = () => {
+    window.scrollTo(0,0);
+    navigate("/contact");
+  }
+
   return (
     <section className={`${styles.contact} ${styles.padding_2}`}>
       <h2 className={`${styles.section__header} ${styles.col_md_6}`}>
@@ -11,7 +17,10 @@ function CTA() {
       <hr
         className={`${styles.section__break} ${styles.section__break_3} ${styles.d_none} ${styles.d_md_block}`}
       />
-      <Link to="/contact" className={`${styles.button} ${styles.button_secondary} ${styles.CTA}`}>
+      <Link 
+        to="/contact"
+        onClick={handleContactLinkClick} 
+        className={`${styles.button} ${styles.button_secondary} ${styles.CTA}`}>
         Contact Me
       </Link>
       
